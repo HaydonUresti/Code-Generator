8aa33cd642135c38927b325c9050b11e9c487538
HaydonUresti <97619164+HaydonUresti@users.noreply.github.com>
2023-02-22 09:15:18 -0700

    Started styling

--- code-generator/src/App.js
+++ code-generator/src/App.js
@@ -2,13 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // returns the html conponents of the web app
   return (
     <div className="App">
-      <header className="App-header">
+      
+        <header className="App-header">
+          This is going to be the header
+          <a className='App-link' href='https://www.youtube.com/watch?v=xvFZjo5PgG0'>link 1</a>
+          <a className='App-link' href='https://www.youtube.com/watch?v=xvFZjo5PgG0'>link 2</a>
+        </header>
+
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Adding a paragraph
+        
+        </p>
+        <button className="App-body">A button</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +32,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <body className='App-body'>
+        <p>This is the</p>
+        <p>This is the</p>
+      </body>
+      <footer className='App-footer'>This is the footer</footer>
     </div>
   );
 }
